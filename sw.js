ded7753e426ab3f1063a4583878c1869691dd36c
Hamed22G <hamedhajihamed00@gmail.com>
2026-06-24 00:22:25 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,12 +1,22 @@
-const CACHE = 'mcdo-shifts-v1';
-const FILES = ['/', '/index.html', '/manifest.json'];
+const CACHE = 'mcdo-shifts-v2';
+
+const FILES = [
+  '/Maccie/',
+  '/Maccie/index.html',
+  '/Maccie/manifest.json',
+  '/Maccie/icon.png'
+];
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(FILES))
+  );
 });
 
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
   );
 });
